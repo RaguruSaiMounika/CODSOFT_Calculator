d4d0b057bd9d67525a6a6aa23be4bd144d1ab29a
RaguruSaiMounika <112754425+RaguruSaiMounika@users.noreply.github.com>
2025-03-23 14:50:45 +0530

    uploaded CODSOFT Calculator

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// Function to add numbers/operators to the display
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+// Function to calculate the result
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
